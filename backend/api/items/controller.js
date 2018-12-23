bcf5f93352860421644d12be0e57e0d064521a39
Francisco Aranda <faranda@gmail.com>
2018-12-23 15:25:47 +0800

    [BE] [calendar-api-crud] update, delete

--- backend/api/items/controller.js
+++ backend/api/items/controller.js
@@ -6,11 +6,20 @@
  */
 
 /* resources: 
- https://www.diycode.cc/projects/google/google-api-nodejs-client
- https://github.com/wanasit/google-calendar/blob/master/example/list-example.js#L17
+-https://developers.google.com/calendar/quickstart/nodejs
+-https://www.diycode.cc/projects/google/google-api-nodejs-client
+-https://github.com/wanasit/google-calendar/blob/master/example/list-example.js#L17
  */
 
 //https://github.com/gsuitedevs/node-samples/issues/6
+
+/* 
+TODO: 
+-Standarize json response, to be encapsulated in the response service
+-Standarize response http codes, as constants in the response service
+-Investigate if there is a way to refactor the code in order to do not repeat the authclient initialization,
+-But it's necessary to be initialize in every request, as it depends on the user access token
+*/
 import { google } from 'googleapis';
 import { google as gconfig } from '../../config';
 import { success, notFound, error } from '../../services/response/';
@@ -37,36 +46,34 @@ export const index = (
   });
 
   const calendar = google.calendar({ version: 'v3', auth: authclient });
+  const object = {
+    calendarId: user.calendarId,
+    timeMin: new Date().toISOString(),
+    maxResults: 16,
+    singleEvents: true,
+    orderBy: 'startTime'
+  };
+  console.log('LIST EVENTS', object);
+  calendar.events.list(object, (err, result) => {
+    if (err) {
+      //TODO: use the default response message format to send errors
+      console.log(err);
+      res.status(409).json({
+        message: 'There was an error contacting the Calendar service'
+      });
 
-  calendar.events.list(
-    {
-      calendarId: user.calendarId,
-      timeMin: new Date().toISOString(),
-      maxResults: 16,
-      singleEvents: true,
-      orderBy: 'startTime'
-    },
-    (err, result) => {
-      if (err) {
-        //TODO: use the default response message format to send errors
-        console.log(err);
-        res.status(409).json({
-          message: 'There was an error contacting the Calendar service'
-        });
+      return;
+    }
 
-        return;
-      }
+    const events = result.data.items;
 
-      const events = result.data.items;
-
-      /*events.map((event, i) => {
+    /*events.map((event, i) => {
         const start = event.start.dateTime || event.start.date;
         console.log(`${start} - ${event.summary}`);
       });*/
 
-      res.json(events);
-    }
-  );
+    res.json(events);
+  });
 };
 
 export const calendar = ({ body, user }, res, next) => {
@@ -80,9 +87,6 @@ export const calendar = ({ body, user }, res, next) => {
 
   calendar.calendarList.list({}, (err, result) => {
     if (err) {
-      //TODO: use the default response message format to send errors
-      //Handle the headers case
-      //UnhandledPromiseRejectionWarning: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
       res.status(409).json({
         message: 'There was an error contacting the Calendar service'
       });
@@ -128,31 +132,88 @@ export const insert = ({ body, user }, res, next) => {
 
   const calendar = google.calendar({ version: 'v3', auth: authclient });
   const event = JSON.parse(body.event);
+  const object = {
+    auth: authclient,
+    calendarId: body.calendarId || user.calendarId,
+    resource: event
+  };
+  console.log('INSERTING EVENT', object);
+  calendar.events.insert(object, function(err, createdEvent) {
+    if (err) {
+      res.status(409).json({
+        message: 'There was an error contacting the Calendar service'
+      });
 
-  calendar.events.insert(
-    {
-      auth: authclient,
-      calendarId: body.calendarId,
-      resource: event
-    },
-    function(err, createdEvent) {
-      if (err) {
-        //TODO: use the default response message format to send errors
-        res.status(409).json({
-          message: 'There was an error contacting the Calendar service'
-        });
-
-        return;
-      }
-
-      res.json(createdEvent.data);
+      return;
     }
-  );
+
+    res.json(createdEvent.data);
+  });
 };
 
 export const show = ({ params }, res, next) => res.status(200).json({});
 
-export const update = ({ body, params }, res, next) =>
-  res.status(200).json(body);
+export const update = ({ body, params, user }, res, next) => {
+  authclient.setCredentials({
+    access_token: user.accessToken,
+    refresh_token: user.accessToken,
+    expiry_date: true
+  });
 
-export const destroy = ({ params }, res, next) => res.status(204).end();
+  const calendar = google.calendar({ version: 'v3', auth: authclient });
+
+  const event = JSON.parse(body.event);
+
+  const object = {
+    auth: authclient,
+    calendarId: user.calendarId || body.calendarId,
+    eventId: params.id || body.eventId,
+    resource: event
+  };
+  console.log('UPDATING EVENT', object);
+  calendar.events.update(object, function(err, updatedEvent) {
+    if (err) {
+      res.status(409).json({
+        message: 'There was an error contacting the Calendar service',
+        error: err.message
+      });
+
+      return;
+    }
+
+    res.json(updatedEvent.data);
+  });
+};
+
+export const destroy = ({ body, params, user }, res, next) => {
+  authclient.setCredentials({
+    access_token: user.accessToken,
+    refresh_token: user.accessToken,
+    expiry_date: true
+  });
+
+  const calendar = google.calendar({ version: 'v3', auth: authclient });
+
+  const object = {
+    auth: authclient,
+    calendarId: user.calendarId || body.calendarId,
+    eventId: params.id || body.eventId
+  };
+
+  console.log('DELETING EVENT', object);
+
+  calendar.events.delete(object, function(err, response) {
+    if (err) {
+      res.status(409).json({
+        message: 'There was an error contacting the Calendar service',
+        error: err.message
+      });
+
+      return;
+    }
+
+    res.status(200).json({
+      message: 'Resource has been deleted'
+    });
+  });
+};
